4366c8d1909e9ff00f0b3be0b74b5dbf2dd4fa02
kurtreyn <kurtreyn@yahoo.com>
2023-12-16 11:27:08 -0500

    update url and localhost

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,7 +11,7 @@ export default function Form() {
   const randomId = uuidv4();
   const date = new Date().toLocaleDateString('en-US');
   // eslint-disable-next-line no-unused-vars
-  const localBackendHost = 'http://localhost:3000/api';
+  // const localBackendHost = 'http://127.0.0.1:3000/api';
   const backendHost = 'https://preact-demo-backend.onrender.com/api';
 
   const formData = {
