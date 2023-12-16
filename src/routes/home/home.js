08b1967319301a7002df569a340caedd6f9e9a06
kurtreyn <kurtreyn@yahoo.com>
2023-12-16 10:48:28 -0500

    added wakeup route for server

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,8 +1,33 @@
 // import { h } from 'preact';
+import { useEffect } from 'preact/hooks';
 import Form from '../../components/Form/Form';
 import style from './style.css';
 
 const Home = () => {
+  // const localBackendHost = 'http://localhost:3000/wakeup';
+  const backendHost = 'https://preact-demo-backend.onrender.com/wakeup';
+
+  async function wakeUpServer(url = backendHost, data = 'wakeUpPost') {
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      body: JSON.stringify(data),
+    })
+      .then((result) => {
+        console.log(result.json());
+      })
+      .catch((error) => {
+        console.log('Errors:', error);
+      });
+  }
+
+  useEffect(() => {
+    wakeUpServer();
+  }, []);
+
   return (
     <div class={style.home_container}>
       <div className="text_wrapper">
@@ -13,63 +38,8 @@ const Home = () => {
         </span>
       </div>
       <Form />
-      {/* <a href="https://preactjs.com">
-        <img
-          src="../../assets/preact-logo.svg"
-          alt="Preact Logo"
-          height="160"
-          width="160"
-        />
-      </a>
-      <h1>Get Started Building PWAs with Preact-CLI</h1>
-      <section>
-        <Resource
-          title="Learn Preact"
-          description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-          link="https://preactjs.com/tutorial/"
-        />
-        <Resource
-          title="Differences to React"
-          description="If you're coming from React, check out our docs for where Preact differs"
-          link="https://preactjs.com/guide/v10/differences-to-react"
-        />
-        <Resource
-          title="Learn Preact-CLI"
-          description="To learn more about Preact-CLI, read through the ReadMe & Wiki"
-          link="https://github.com/preactjs/preact-cli#preact-cli--"
-        />
-      </section> */}
     </div>
   );
 };
 
-// const Resource = (props) => {
-//   return (
-//     <a href={props.link} class={style.resource}>
-//       <h2>{props.title}</h2>
-//       <p>{props.description}</p>
-//     </a>
-//   );
-// };
-
 export default Home;
-
-/*
-import './style';
-import Form from './components/Form/Form';
-
-export default function App() {
-  return (
-    <div class="app_container">
-      <div className="text_wrapper">
-        <h1 className="header_text">Welcome!</h1>
-        <span class="text">
-          Please fill out the form and we'll send an email to you with a PDF
-          attachment, thanking you for your visit.
-        </span>
-      </div>
-      <Form />
-    </div>
-  );
-}
-*/
